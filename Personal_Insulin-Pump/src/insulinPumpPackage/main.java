18d9d8de47c02722a6236682c4fa2a03d5dae18f
JosephPawlowski1 <joepawlowski712@gmail.com>
2020-02-25 20:21:15 -0600

    finished battery level and insulin level

--- Personal_Insulin-Pump/src/insulinPumpPackage/main.java
+++ Personal_Insulin-Pump/src/insulinPumpPackage/main.java
@@ -7,12 +7,14 @@ public class main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner scanObj = new Scanner(System.in);
+		System.out.println("Battery level and Insulin level");
+		do {
 		System.out.println("Enter Battery level");
 		int batteryLevel = scanObj.nextInt();
 		System.out.println("Enter Insulin Level");
 		int insulinLevel = scanObj.nextInt();
 		insulinPump ip = new insulinPump(batteryLevel,insulinLevel);
-		System.out.println(ip.getBatteryLevel());
+		System.out.println("Battery Level: " + ip.getBatteryLevel());
 		if(ip.alarmBatteryLevel())
 		{
 			System.out.println("Battery Low");
@@ -21,15 +23,17 @@ public class main {
 		{
 			System.out.println("Battery Fine");
 		}
-		System.out.println(ip.getInsulinLevel());
+		System.out.println("Insulin Level: " + ip.getInsulinLevel());
 		if(ip.alarmInsulinLevel())
-		{
-			System.out.println("Insulin Low");
-		}
+			{
+				System.out.println("Insulin Low");
+			}
 		else 
-		{
-			System.out.println("Insulin Fine");
-		}
+			{
+				System.out.println("Insulin Fine");
+			}
+		System.out.println("Enter 0 to quit or another number to keep going.");
+		}while(scanObj.nextInt()!=0);
 	}
 
 }
